64b0e2efb9195ee71669182d86299e286ca7bf5e
MrBacon470 <Mrbacon470@outlook.com>
2023-02-23 16:33:09 -0600

    New Website!

--- JS/data.js
+++ JS/data.js
@@ -6,12 +6,78 @@ const projectObjs = [
         state: 'Finished?',
         lang: 'C#',
         platform: 'Downloadable',
-        stateColor: '--green',
+        titleColor: '--green'
     },
     {
         name: 'Power Idle',
         desc: 'A Simple Game about making lots of power',
         url: 'https://mrbacon470.itch.io/power-idle',
-        state: 'Finished'
+        state: 'Finished?',
+        lang: 'C#',
+        platform: 'Downloadable',
+        titleColor: '--yellow'
+    },
+    {
+        name: 'Atom Smasher Idle',
+        desc: 'Prototype game that was like Omega Layers',
+        url: 'https://mrbacon470.itch.io/atom-smasher-idle',
+        state: 'Prototype',
+        lang: 'C#',
+        platform: 'Downloadable',
+        titleColor: '--orange'
+    },
+    {
+        name: 'Chemcremental (Original)',
+        desc: 'A chemistry themed idle game',
+        url: 'https://mrbacon470.itch.io/chemcremental',
+        state: 'Finished',
+        lang: 'C#',
+        platform: 'Downloadable & Web',
+        titleColor: '--blue'
+    },
+    {
+        name: 'Pay 2 Win Eternal',
+        desc: 'April Fools Joke Game about p2w games',
+        url: 'https://mrbacon470.github.io/P2W-Eternal-Web/',
+        state: 'Finished?',
+        lang: 'C#',
+        platform: 'Web',
+        titleColor: '--yellow'
+    },
+    {
+        name: 'Chemcremental JS Remaster',
+        desc: 'Chemcremental but better and in JS',
+        url: 'https://mrbacon470.github.io/Chemcremental-JS/',
+        state: 'Finished',
+        lang: 'JS',
+        platform: 'Web',
+        titleColor: '--blue'
+    },
+    {
+        name: 'Incremental Army',
+        desc: 'A bad incremental prototype about building armies',
+        url: 'https://mrbacon470.github.io/Incremental-Army/',
+        state: 'Prototype',
+        lang: 'JS',
+        platform: 'Web',
+        titleColor: '--bluegreen'
+    },
+    {
+        name: 'Coop Co',
+        desc: 'Egg Inc Knockoff but without all the MTX and its fun',
+        url: 'https://mrbacon470.github.io/Coop-Co/',
+        state: 'Being Updated Still',
+        lang: 'JS',
+        platform: 'Web',
+        titleColor: '--yellow'
+    },
+    {
+        name: 'Inactive Testing',
+        desc: 'Knockoff of Cryptogrounds Idle Research but its just a prototype and not fun',
+        url: 'https://mrbacon470.github.io/Inactive-Testing/',
+        state: 'Prototype',
+        lang: 'JS',
+        platform: 'Web',
+        titleColor: '--purple'
     }
 ]
--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,25 @@ let baseHTMLStr = `
 <p>Made with [Lang &| Engine] | Platform: [Platform]</p>
 <p>State</p>
 </div>`
+function generateWebpageHTML() {
+    for(let i = 0; i < projectObjs.length / 3; i++) {
+        let rowHTMLStr = `
+        <div id="row${i}" class="flexRow"></div>`
+        document.getElementById('mainContainer').innerHTML += rowHTMLStr
+        for(let j = 0; j < 3; j++) {
+            if(i * 3 + j >= projectObjs.length) break
+            let project = projectObjs[i * 3 + j]
+            let htmlStr = `
+            <div class="projectContainer" onclick="window.open('${project.url}')">
+            <h4 style="color: var(${project.titleColor})">${project.name}</h4>
+            <p>${project.desc}</p>
+            <p>Made with ${project.lang} | Platform: ${project.platform}<br>Status: ${project.state}</p>`
+
+            document.getElementById(`row${i}`).innerHTML += htmlStr
+        }
+    }
+}
+
+window.onload = function() {
+    generateWebpageHTML()
+}
